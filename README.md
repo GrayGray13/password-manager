aa5ffe000560457e417571297e3345a5c226a9d8
Grayson Dabbs <graysondabbs13@gmail.com>
2023-05-30 14:35:39 -0400

    added search function and changed data from .txt to .json

--- README.md
+++ README.md
@@ -3,8 +3,10 @@
 ## About
 - This password manager app is meant to store passwords locally on your computer
 - It was made using Python and Tkinter
-- Once you fill out the form, you can submit the data, and it will append the password to a local password list: [data.txt](data.txt)
-- It can also generate a random password and then copy the password to your clipboard once generated
+- Once you fill out the form, you can submit the data, and it will append the password to a local password list or create a new one if the file doesn't already exist
+- It can generate a random password and then copy the password to your clipboard once generated
+- It has exception handling for the data file
+- You can use the search button to search through your password list for a certain website's password that you saved
 
 ## Author
 - Grayson Dabbs
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -30,23 +31,50 @@ def save():
     website = website_entry.get()
     username = user_entry.get()
     password = pass_entry.get()
+    new_data = {
+        website: {
+            "username": username,
+            "password": password
+        }
+    }
 
     if website == "" or username == "" or password == "":
         messagebox.showinfo(title="Missing Data", message="There is missing data for the password\nPlease "
                                                           "try again.")
     else:
-        is_ok = messagebox.askokcancel(title=website,
-                                       message=f"These are the details entered for {website}:\nUsername: {username}\n"
-                                               f"Password: {password}\nWould you like to save?")
-
-        if is_ok:
-            data = f"{website} | {username} | {password}\n"
-            with open("data.txt", mode="a") as file:
-                file.write(data)
+        data = f"{website} | {username} | {password}\n"
+        try:
+            with open("data.json", mode="r") as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", mode="w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            data.update(new_data)
+            with open("data.json", mode="w") as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
             website_entry.delete(0, END)
             user_entry.delete(0, END)
             user_entry.insert(0, "test@gmail.com")
             pass_entry.delete(0, END)
+
+
+# --------------------------- Find Password ------------------------------- #
+def find_password():
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found")
+    else:
+        website = website_entry.get()
+        if data.get(website) is None:
+            messagebox.showinfo(title="Error", message="No details for the website exists")
+        else:
+            username = data.get(website)["username"]
+            password = data.get(website)["password"]
+            messagebox.showinfo(title=website, message=f"Username: {username}\nPassword: {password}")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -62,22 +90,24 @@ website_label = Label(text="Website:")
 user_label = Label(text="Email/Username:")
 pass_label = Label(text="Password:")
 
-website_entry = Entry()
+website_entry = Entry(width=30)
 user_entry = Entry()
 pass_entry = Entry(width=30)
 
+search_button = Button(text="Search", command=find_password)
 pass_gen_button = Button(text="Generate Password", command=generate_password)
 add_button = Button(text="Add", width=30, command=save)
 
 canvas.grid(column=1, row=0)
 website_label.grid(column=0, row=1)
-website_entry.grid(columnspan=2, column=1, row=1, padx=5, pady=2, sticky="ew")
+website_entry.grid(column=1, row=1, padx=5, sticky="ew")
 website_entry.focus()
+search_button.grid(column=2, row=1, padx=5, pady=2, sticky="ew")
 user_label.grid(column=0, row=2)
 user_entry.grid(columnspan=2, column=1, row=2, padx=5, pady=2, sticky="ew")
 user_entry.insert(0, "test@gmail.com")
 pass_label.grid(column=0, row=3, sticky="ew")
-pass_entry.grid(column=1, row=3, padx=5, sticky="w")
+pass_entry.grid(column=1, row=3, padx=5, sticky="ew")
 pass_gen_button.grid(column=2, row=3, padx=5, pady=2, sticky="ew")
 add_button.grid(column=1, columnspan=2, row=4, padx=5, pady=2, sticky="ew")
 
